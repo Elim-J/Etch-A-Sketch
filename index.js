1ac2c63a43348586309e2a363bfb8937a2cf83fa
Sabrina Kramer <01CodePathways11@gmail.com>
2022-07-14 09:55:08 -0500

    Create changeColor() for squares

--- index.js
+++ index.js
@@ -4,12 +4,26 @@
             Could add an input box for this
 
  */     
+    function changeColor(e){
+        console.log(e);
+        let squareId = e.target.id;
+        let square = document.getElementById(squareId);
+        square.style.cssText = "background-color: red;";
+    }
+
+    /*
+    Return number 0-255
+    */
+    function getRandomNumber(){
+        return Math.floor((Math.random() * 256));
+    }
 
     const root = document.querySelector("#root");
 
     //Create a container with a class name
     const container = document.createElement("div");
     container.classList.add("container");
+    //container.addEventListener("mouseover", changeColor);
     
     let numOfRows = 16  //start with hardcoded number for now
     let numSquares = numOfRows * numOfRows; 
@@ -18,12 +32,28 @@
         //give divs a class name [so you can style them in css]
         let square = document.createElement("div");
         square.classList.add("square");
+        square.setAttribute("id", i);
+        square.addEventListener("mouseover", changeColor);
         //append the divs to the container
         container.appendChild(square);
     }
-        
+
     //Append the container to root
     root.appendChild(container);
+/*
+        Figure out how to change color of divs permanently on hover
+            Get the container's children (all of them), add
+            an event listener to them (hover event) and then add a function
+            which I write at the top which changes their background color
+
+            updated: try to make it change to different colors
+            final update: try to make it 10% of black so that after
+                10 passes it is completely black.
+    */
+
+    
+
+    
 
 
 
