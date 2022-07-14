aead7afc0170e8a36f8d037cdc64823e2427e299
Sabrina Kramer <01CodePathways11@gmail.com>
2022-07-14 13:30:56 -0500

    Refactor code and comments

--- index.js
+++ index.js
@@ -1,70 +1,82 @@
-/*
-    Create a webpage with a 16x16 grid of square divs
-        [I want the user to be able to choose the size of etch-n-sketch]
-            Could add an input box for this
 
- */   
-    const squareColor = setSquareColor();
-    /*
-        Return number 0-255
-    */
+/**
+ * Etch-N-Sketch
+ * Sabrina K.
+ * 
+ * Project Description:
+ *      40x40 grid that users can draw on
+ *      Initializes single color for all squares and decreases brightness on each mouseover
+ *      
+ * Additional Comments:
+ *      etch-customizing-func.js contains functionality for random colors
+ *      May create functionality for user to change color in future design
+ */
+   
+    /**
+     * 
+     * @returns number 0-255 for rgb
+     */
     function getRandomNumber(){
         return Math.floor((Math.random() * 256));
     }
 
-    // Generate one random color for all squares
-        //return string
-    function setSquareColor(){
+    /**
+     * 
+     * @returns string style property
+     */
+    function generateColor(){
          return `background-color: rgb(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()})`;
     }
 
-    function assignInitialColor(e){
+    /**
+     * 
+     * @param {mouseover} e 
+     */
+    function setSquareColor(e){
         let squareId = e.target.id;
         let square = document.getElementById(squareId);
         square.style.cssText = squareColor;
-        square.removeEventListener("mouseover", assignInitialColor);
+
+        square.removeEventListener("mouseover", setSquareColor);
         square.addEventListener("mouseover", decreaseBrightness);
     }
 
-    /*
-        Note about brightness: Red-Green-Blue: the higher the number, the brighter it becomes
-            rbg(0, 10, 0) --> black with some green in it
-            I could use this fact to decrease brightness by decreasing rgb values
-            */
+    /**
+     * 
+     * @param {mouseover} e 
+     */
     function decreaseBrightness(e){
-        //numRegex matches all numbers and spaces
             let squareId = e.target.id;
             let square = document.getElementById(squareId);
-            let numRegex = /\d+|s/g;
-            let numbers = square.style.cssText.match(numRegex);
-            let num1 = numbers[0] - 25;
-            let num2 = numbers[1] - 25;
-            let num3 = numbers[2] - 25;
-            square.style.cssText = `background-color: rgb(${num1}, ${num2}, ${num3})`
+
+            let numRegex = /\d+|s/g;    //match all numbers and spaces
+            let num = square.style.cssText.match(numRegex);     //store rgb values in num
+            square.style.cssText = `background-color: rgb(${num[0] - 25}, ${num[1] - 25}, ${num[2] - 25})`;
     }
 
+    const squareColor = generateColor();
     const root = document.querySelector("#root");
 
-    //Create a container with a class name
+    //Container holds all squares
     const container = document.createElement("div");
     container.classList.add("container");
     
-    let numOfRows = 16  //start with hardcoded number for now
+    let numOfRows = 40;
     let numSquares = numOfRows * numOfRows; 
 
-    //create squares
+    /**
+     * 
+     * Create Squares, Append to container
+     */
     for(let i = 0; i < numSquares; i++){
-        //give divs a class name [so you can style them in css]
         let square = document.createElement("div");
         square.classList.add("square");
         square.setAttribute("id", i);
-        square.addEventListener("mouseover", assignInitialColor, {once: true});
-        
-        //append the divs to the container
+        square.addEventListener("mouseover", setSquareColor, {once: true});
         container.appendChild(square);
     }
 
-    //Append the container to root
+    //Finally, append container with all squares to root
     root.appendChild(container);
 
     
