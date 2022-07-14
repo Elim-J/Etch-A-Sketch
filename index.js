f0c7996e33ea7ce7bb8e311b5576e71817e44da0
Sabrina Kramer <01CodePathways11@gmail.com>
2022-07-14 10:24:27 -0500

    Use getRandomNumber() to generate random colors

--- index.js
+++ index.js
@@ -3,20 +3,36 @@
         [I want the user to be able to choose the size of etch-n-sketch]
             Could add an input box for this
 
- */     
-    function changeColor(e){
-        console.log(e);
-        let squareId = e.target.id;
-        let square = document.getElementById(squareId);
-        square.style.cssText = "background-color: red;";
-    }
+ */   
 
     /*
-    Return number 0-255
+        Return number 0-255
     */
     function getRandomNumber(){
         return Math.floor((Math.random() * 256));
     }
+
+    function changeColor(e){
+        console.log(e);
+        let squareId = e.target.id;
+        let square = document.getElementById(squareId);
+        square.style.cssText = `background-color: red;`;
+
+        //Note to self: Needed +=, otherwise background-color:red 
+            //will just be overwritten by brightness
+            //Question: How to dynamically change brightness? 
+                //and subtract brightness by 10%?
+        square.style.cssText += `filter: brightness(50%);`;
+    }
+
+    function changeColorRainbow(e){
+        console.log(e);
+        let squareId = e.target.id;
+        let square = document.getElementById(squareId);
+        square.style.cssText = `background-color: rgb(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()});`;
+    }
+
+   
 
     const root = document.querySelector("#root");
 
@@ -25,7 +41,7 @@
     container.classList.add("container");
     //container.addEventListener("mouseover", changeColor);
     
-    let numOfRows = 16  //start with hardcoded number for now
+    let numOfRows = 2  //start with hardcoded number for now
     let numSquares = numOfRows * numOfRows; 
 
     for(let i = 0; i < numSquares; i++){
@@ -40,16 +56,6 @@
 
     //Append the container to root
     root.appendChild(container);
-/*
-        Figure out how to change color of divs permanently on hover
-            Get the container's children (all of them), add
-            an event listener to them (hover event) and then add a function
-            which I write at the top which changes their background color
-
-            updated: try to make it change to different colors
-            final update: try to make it 10% of black so that after
-                10 passes it is completely black.
-    */
 
     
 
